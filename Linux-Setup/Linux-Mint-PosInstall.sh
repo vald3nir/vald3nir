e9ec1eb48a20ed958ab3755c2f66c94aa1df2492
Valdenir Severino <vald3nir@gmail.com>
2022-09-10 12:24:38 -0300

    Update Linux-Mint-PosInstall.sh

--- Linux-Setup/Linux-Mint-PosInstall.sh
+++ Linux-Setup/Linux-Mint-PosInstall.sh
@@ -1,118 +1,23 @@
 #!/usr/bin/env bash
-# ----------------------------- VARIÁVEIS ----------------------------- #
-PPA_LIBRATBAG="ppa:libratbag-piper/piper-libratbag-git"
-PPA_LUTRIS="ppa:lutris-team/lutris"
-PPA_GRAPHICS_DRIVERS="ppa:graphics-drivers/ppa"
 
-URL_WINE_KEY="https://dl.winehq.org/wine-builds/winehq.key"
-URL_PPA_WINE="https://dl.winehq.org/wine-builds/ubuntu/"
-URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-URL_SIMPLE_NOTE="https://github.com/Automattic/simplenote-electron/releases/download/v1.8.0/Simplenote-linux-1.8.0-amd64.deb"
-URL_4K_VIDEO_DOWNLOADER="https://dl.4kdownload.com/app/4kvideodownloader_4.9.2-1_amd64.deb"
-URL_INSYNC="https://d2t3ff60b2tol4.cloudfront.net/builds/insync_3.0.20.40428-bionic_amd64.deb"
-
-DIRETORIO_DOWNLOADS="$HOME/Downloads/programas"
-
-PROGRAMAS_PARA_INSTALAR=(
-  snapd
-  mint-meta-codecs
-  winff
-  guvcview
-  virtualbox
-  flameshot
-  nemo-dropbox
-  steam-installer
-  steam-devices
-  steam:i386
-  ratbagd
-  piper
-  lutris
-  libvulkan1
-  libvulkan1:i386
-  libgnutls30:i386
-  libldap-2.4-2:i386
-  libgpg-error0:i386
-  libxml2:i386
-  libasound2-plugins:i386
-  libsdl2-2.0-0:i386
-  libfreetype6:i386
-  libdbus-1-3:i386
-  libsqlite3-0:i386
-)
-# ---------------------------------------------------------------------- #
-
-# ----------------------------- REQUISITOS ----------------------------- #
-## Removendo travas eventuais do apt ##
+# Removing occasional locks from apt
 sudo rm /var/lib/dpkg/lock-frontend
 sudo rm /var/cache/apt/archives/lock
-
-## Adicionando/Confirmando arquitetura de 32 bits ##
+# Adding/Confirming 32-bit architecture
 sudo dpkg --add-architecture i386
-
-## Atualizando o repositório ##
+# Enable SNAP
+sudo rm /etc/apt/preferences.d/nosnap.pref
 sudo apt update -y
+sudo apt install snapd -y
 
-## Adicionando repositórios de terceiros e suporte a Snap (Driver Logitech, Lutris e Drivers Nvidia)##
-sudo apt-add-repository "$PPA_LIBRATBAG" -y
-sudo add-apt-repository "$PPA_LUTRIS" -y
-sudo apt-add-repository "$PPA_GRAPHICS_DRIVERS" -y
-wget -nc "$URL_WINE_KEY"
-sudo apt-key add winehq.key
-sudo apt-add-repository "deb $URL_PPA_WINE bionic main"
-# ---------------------------------------------------------------------- #
+# Download and install third-party software
+DIRETORIO_DOWNLOADS="$HOME/Downloads/programas"
+# VS Code
+wget -P "$DIRETORIO_DOWNLOADS" -c https://az764295.vo.msecnd.net/stable/784b0177c56c607789f9638da7b6bf3230d47a8c/code_1.71.0-1662018389_amd64.deb 
+# VS Code
+wget -P "$DIRETORIO_DOWNLOADS" -c https://objects.githubusercontent.com/github-production-release-asset-2e65be/85733855/f2382800-5c01-11e9-9f4b-a4854ea802c1?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20220908%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220908T123746Z&X-Amz-Expires=300&X-Amz-Signature=9a5d08b67cdd06b1424b56b9f5039dd5879124f1e8d902af1f56aa98e41bd396&X-Amz-SignedHeaders=host&actor_id=0&key_id=0&repo_id=85733855&response-content-disposition=attachment%3B%20filename%3Ddockstation_1.5.1_amd64.deb&response-content-type=application%2Foctet-stream 
 
-# ----------------------------- EXECUÇÃO ----------------------------- #
-## Atualizando o repositório depois da adição de novos repositórios ##
-sudo apt update -y
-
-## Download e instalaçao de programas externos ##
-mkdir "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_GOOGLE_CHROME"       -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_SIMPLE_NOTE"         -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_4K_VIDEO_DOWNLOADER" -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_INSYNC"              -P "$DIRETORIO_DOWNLOADS"
-
-## Instalando pacotes .deb baixados na sessão anterior ##
 sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
-
-# Instalar programas no apt
-for nome_do_programa in ${PROGRAMAS_PARA_INSTALAR[@]}; do
-  if ! dpkg -l | grep -q $nome_do_programa; then # Só instala se já não estiver instalado
-    apt install "$nome_do_programa" -y
-  else
-    echo "[INSTALADO] - $nome_do_programa"
-  fi
-done
-
-sudo apt install --install-recommends winehq-stable wine-stable wine-stable-i386 wine-stable-amd64 -y
-
-## Instalando pacotes Flatpak ##
-flatpak install flathub com.obsproject.Studio -y
-
-## Instalando pacotes Snap ##
-sudo snap install spotify
-sudo snap install slack --classic
-sudo snap install skype --classic
-sudo snap install photogimp
-# ---------------------------------------------------------------------- #
-
-# ----------------------------- PÓS-INSTALAÇÃO ----------------------------- #
-## Finalização, atualização e limpeza##
-sudo apt update && sudo apt dist-upgrade -y
-flatpak update
-sudo apt autoclean
-sudo apt autoremove -y
-# ---------------------------------------------------------------------- #
-
-
-
-
-
-
-
-
-
-
 
 # Installing github-desktop
 wget -qO - https://packagecloud.io/shiftkey/desktop/gpgkey | sudo tee /etc/apt/trusted.gpg.d/shiftkey-desktop.asc > /dev/null
@@ -120,41 +25,43 @@ sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/
 sudo apt-get update
 sudo apt install github-desktop
 
+# Removing the software not used
+# sudo apt purge firefox firefox-locale-* -y
+sudo apt purge thunderbird celluloid hexchat hypnotix rhythmbox -y
+
+# Installing APT packages
+sudo apt install samba samba-common-bin -y
+sudo apt install git curl fish -y
+sudo apt install build-essential -y
+sudo apt install python3-pip -y
+sudo apt install arduino -y
+sudo apt install vlc clementine -y
+sudo apt install gparted -y
+sudo apt install sqlitebrowser -y
+# sudo apt install gimp -y
+# sudo apt install filezilla -y
+# sudo apt install hollywood -y
+
+# Installing Snap packages
+sudo snap install snap-store
+sudo snap install docker
+sudo snap install android-studio --classic
+sudo snap install flutter --classic
+sudo snap install pycharm-community --classic
+# sudo snap install postman
+# sudo snap install robo3t-snap
+# sudo snap install dotnet-sdk --classic --channel=5.0
+# sudo snap install eclipse --classic
+# sudo snap install intellij-idea-community --classic
+# sudo snap install spotify
+# sudo snap install slack --classic
+# sudo snap install skype --classic
+
 # Setup SSH
 sudo apt install openssh-server
 sudo ufw allow ssh
 
-# Installing SNAP
-sudo rm /etc/apt/preferences.d/nosnap.pref
-sudo apt install nodejs snapd
-
-# Removing the software not used
-sudo apt purge firefox firefox-locale-* thunderbird celluloid hexchat hypnotix rhythmbox -y
-
-# Installing core software
-
-# sudo apt install python3-pip -y
-sudo apt install nodejs npm -y
-sudo npm install -g --unsafe-perm node-red 
-sudo apt install arduino -y
-sudo apt install build-essential -y
-sudo apt install git curl fish -y
-sudo apt install gimp -y
-sudo apt install vlc clementine -y
-sudo apt install gparted -y
-sudo apt install sqlitebrowser -y
-sudo snap install docker
-sudo snap install android-studio --classic
-sudo snap install flutter --classic
-sudo snap install postman
-sudo snap install code --classic
-sudo snap install robo3t-snap
-sudo snap install pycharm-community --classic
-# sudo snap install dotnet-sdk --classic --channel=5.0
-# sudo snap install eclipse --classic
-# sudo snap install intellij-idea-community --classic
-# sudo apt install filezilla -y
-# sudo apt install hollywood -y
-
-# Update libraries
-sudo apt-get update && sudo apt-get upgrade -y
+## Finalização, atualização e limpeza##
+sudo apt update && sudo apt dist-upgrade -y
+flatpak update
+sudo apt autoclean && apt autoremove -y
