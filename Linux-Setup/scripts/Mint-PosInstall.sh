78a3f13540c9c355d9dabc7910e7a5e1ad28ecf6
Valdenir Severino <vald3nir@gmail.com>
2022-11-06 09:35:27 -0300

    Update Mint-PosInstall.sh

--- Linux-Setup/scripts/Mint-PosInstall.sh
+++ Linux-Setup/scripts/Mint-PosInstall.sh
@@ -12,6 +12,34 @@ sudo apt purge firefox firefox-locale-* thunderbird celluloid hexchat hypnotix r
 # Update repositories
 sudo apt update && sudo apt dist-upgrade -y
 
+# Setup SSH
+sudo apt-get install openssh-server -y
+sudo systemctl enable ssh --now
+sudo ufw allow ssh
+
+# Installing github-desktop
+wget -qO - https://packagecloud.io/shiftkey/desktop/gpgkey | sudo tee /etc/apt/trusted.gpg.d/shiftkey-desktop.asc > /dev/null
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/ any main" > /etc/apt/sources.list.d/packagecloud-shiftkey-desktop.list'
+sudo apt-get update 
+sudo apt install github-desktop -y
+
+# Download and install third-party software
+FOLDER_DOWNLOADS="$HOME/Downloads/programas"
+programs=(
+    # VS Code
+    "https://az764295.vo.msecnd.net/stable/784b0177c56c607789f9638da7b6bf3230d47a8c/code_1.71.0-1662018389_amd64.deb"
+    # Microsoft Edge
+    "https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_106.0.1370.52-1_amd64.deb"
+    # VNC Server
+    "https://downloads.realvnc.com/download/file/vnc.files/VNC-Server-6.11.0-Linux-x64.deb"
+    # Docker 
+    "https://desktop.docker.com/linux/main/amd64/docker-desktop-4.13.1-amd64.deb"
+)
+for p in ${programs[@]}; do
+   wget -P "$FOLDER_DOWNLOADS" -c "$p"
+done
+sudo dpkg -i $FOLDER_DOWNLOADS/*.deb
+
 # Enable SNAP
 sudo rm /etc/apt/preferences.d/nosnap.pref
 sudo apt update -y
@@ -42,30 +70,6 @@ sudo apt install vlc -y
 # sudo apt install gimp -y
 # sudo apt install filezilla -y
 # sudo apt install hollywood -y
-
-# Setup SSH
-sudo apt-get install openssh-server -y
-sudo systemctl enable ssh --now
-sudo ufw allow ssh
-
-# Installing github-desktop
-wget -qO - https://packagecloud.io/shiftkey/desktop/gpgkey | sudo tee /etc/apt/trusted.gpg.d/shiftkey-desktop.asc > /dev/null
-sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/ any main" > /etc/apt/sources.list.d/packagecloud-shiftkey-desktop.list'
-sudo apt-get update 
-sudo apt install github-desktop -y
-
-# Download and install third-party software
-FOLDER_DOWNLOADS="$HOME/Downloads/programas"
-programs=(
-    # VS Code
-    "https://az764295.vo.msecnd.net/stable/784b0177c56c607789f9638da7b6bf3230d47a8c/code_1.71.0-1662018389_amd64.deb"
-    # Microsoft Edge
-    "https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_106.0.1370.52-1_amd64.deb"
-)
-for p in ${programs[@]}; do
-   wget -P "$FOLDER_DOWNLOADS" -c "$p"
-done
-sudo dpkg -i $FOLDER_DOWNLOADS/*.deb
 
 # Finish 
 sudo apt update && sudo apt dist-upgrade -y
