1cbcc01aa030d442e0c1913c88408083187f6a59
Valdenir Severino <vald3nir@gmail.com>
2022-11-06 10:44:38 -0300

    Update Mint-PosInstall.sh

--- Linux-Setup/scripts/Mint-PosInstall.sh
+++ Linux-Setup/scripts/Mint-PosInstall.sh
@@ -23,22 +23,19 @@ sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/
 sudo apt-get update 
 sudo apt install github-desktop -y
 
-# Download and install third-party software
-FOLDER_DOWNLOADS="$HOME/Downloads/programas"
-programs=(
-    # VS Code
-    "https://az764295.vo.msecnd.net/stable/784b0177c56c607789f9638da7b6bf3230d47a8c/code_1.71.0-1662018389_amd64.deb"
-    # Microsoft Edge
-    "https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_106.0.1370.52-1_amd64.deb"
-    # VNC Server
-    "https://downloads.realvnc.com/download/file/vnc.files/VNC-Server-6.11.0-Linux-x64.deb"
-    # Docker 
-    "https://desktop.docker.com/linux/main/amd64/docker-desktop-4.13.1-amd64.deb"
-)
-for p in ${programs[@]}; do
-   wget -P "$FOLDER_DOWNLOADS" -c "$p"
-done
-sudo dpkg -i $FOLDER_DOWNLOADS/*.deb
+# Installing Docker
+sudo apt-get -y remove docker.io docker-engine containerd runc
+sudo apt-get update
+sudo apt-get install -y apt-transport-https software-properties-common ca-certificates wget
+wget https://download.docker.com/linux/ubuntu/gpg
+sudo apt-key add gpg
+echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-cache policy docker-ce
+sudo apt-get install docker-ce -y
+sudo systemctl start docker
+sudo systemctl enable docker
+
 
 # Enable SNAP
 sudo rm /etc/apt/preferences.d/nosnap.pref
@@ -46,8 +43,8 @@ sudo apt update -y
 sudo apt install snapd -y
 
 # Installing Snap packages
-# sudo snap install snap-store
 # sudo snap install docker
+# sudo snap install snap-store
 # sudo snap install android-studio --classic
 # sudo snap install flutter --classic
 # sudo snap install pycharm-community --classic
@@ -70,6 +67,23 @@ sudo apt install vlc -y
 # sudo apt install gimp -y
 # sudo apt install filezilla -y
 # sudo apt install hollywood -y
+
+# Download and install third-party software
+FOLDER_DOWNLOADS="$HOME/Downloads/programas"
+programs=(
+    # VS Code
+    "https://az764295.vo.msecnd.net/stable/784b0177c56c607789f9638da7b6bf3230d47a8c/code_1.71.0-1662018389_amd64.deb"
+    # Microsoft Edge
+    "https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_106.0.1370.52-1_amd64.deb"
+    # VNC Server
+    "https://downloads.realvnc.com/download/file/vnc.files/VNC-Server-6.11.0-Linux-x64.deb"
+    # Docker 
+    "https://desktop.docker.com/linux/main/amd64/docker-desktop-4.13.1-amd64.deb"
+)
+for p in ${programs[@]}; do
+   wget -P "$FOLDER_DOWNLOADS" -c "$p"
+done
+sudo dpkg -i $FOLDER_DOWNLOADS/*.deb
 
 # Finish 
 sudo apt update && sudo apt dist-upgrade -y
